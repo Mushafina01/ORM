ecf2a351749598fa23cf649d5df30beb590dcc5d
Mushafina01 <mushafinariyas@velandev.life>
2025-07-09 14:34:27 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+## Review System
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -22,3 +23,24 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.user.username} for {self.product.name}"
+
+
+---
+
+## Movie-Actor Relationship
+from django.db import models
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    actors = models.ManyToManyField(Actor, related_name='movies')
+
+    def __str__(self):
+        return self.title
