f502147b87fb32fece00b4801686e038a01737ac
Mushafina01 <mushafinariyas@velandev.life>
2025-07-09 14:32:04 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from .models import Movie, Actor
+from .models import Product, Review
 
-@admin.register(Actor)
-class ActorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'birth_date')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     search_fields = ('name',)
 
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date')
-    search_fields = ('title',)
-    filter_horizontal = ('actors',)  
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'rating', 'created_at')
+    search_fields = ('user__username', 'product__name')
