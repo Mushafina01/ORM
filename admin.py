09cd4e1ea7d49b116ff1f4bb53a80691b2134b17
Mushafina01 <mushafinariyas@velandev.life>
2025-07-09 14:35:36 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,4 @@
+## Review System
 from django.contrib import admin
 from .models import Product, Review
 
@@ -10,3 +11,20 @@ class ProductAdmin(admin.ModelAdmin):
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'product', 'rating', 'created_at')
     search_fields = ('user__username', 'product__name')
+
+---
+
+## Movie-Actor Relationship 
+from django.contrib import admin
+from .models import Movie, Actor
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date')
+    search_fields = ('title',)
+    filter_horizontal = ('actors',)  
